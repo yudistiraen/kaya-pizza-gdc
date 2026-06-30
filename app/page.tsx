95119702ac3d94754819bb24558e0eabb95ed8c3
yudis <yudistiradoremi@gmail.com>
2026-06-30 10:08:28 +0700

    add vercel speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Order from "@/components/Order";
 import Location from "@/components/Location";
 import Footer from "@/components/Footer";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <Location />
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </main>
   );
 }
